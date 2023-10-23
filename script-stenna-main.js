24c627831f7df57ac5b16ad93a1f5005ffea3f45
swiwidesign <hello@swiwi.me>
2023-10-23 17:42:49 +0200

    Update script-stenna-main.js

--- script-stenna-main.js
+++ script-stenna-main.js
@@ -1,4 +1,51 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  // LENIS
+  "use strict";
+
+  if (Webflow.env("editor") === undefined) {
+    const lenis = new Lenis({
+      duration: 1.2,
+      //easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.1,
+      wheelMultiplier: 0.7,
+      infinite: false,
+      gestureOrientation: "vertical",
+      normalizeWheel: false,
+      smoothTouch: false
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    $("[data-lenis-start]").on("click", function () {
+      lenis.start();
+    });
+    $("[data-lenis-stop]").on("click", function () {
+      lenis.stop();
+    });
+    $("[data-lenis-toggle]").on("click", function () {
+      $(this).toggleClass("stop-scroll");
+      if ($(this).hasClass("stop-scroll")) {
+        lenis.stop();
+      } else {
+        lenis.start();
+      }
+    });
+
+    function connectToScrollTrigger() {
+      lenis.on("scroll", ScrollTrigger.update);
+      gsap.ticker.add((time) => {
+        lenis.raf(time * 1000);
+      })
+        gsap.ticker.lagSmoothing(0);
+    }
+    // Uncomment this if using GSAP ScrollTrigger
+    connectToScrollTrigger();
+  }
+
   gsap.registerPlugin(ScrollTrigger);
 
  
@@ -18,60 +65,57 @@ window.addEventListener("DOMContentLoaded", (event) => {
     
     
 // Dark to Light Color Change
-$("[colourchange]").each(function (index) {
-  let triggerElement = $(this);
-  let targetElement = $(".page_wrap");
+document.querySelectorAll('[colourchange]').forEach(function (triggerElement) {
+  let targetElement = document.querySelector('.page_wrap');
 
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: triggerElement,
-      // trigger element - viewport
-      // can also use "20px 80%"
-      start: "top bottom",
-      end: "bottom bottom",
-      scrub: 1
-    }
+      start: 'top bottom',
+      end: 'bottom bottom',
+      scrub: 1,
+    },
   });
+
   tl.fromTo(
-    targetElement,
+    targetElement.style,
     {
-      backgroundColor: "#ffffff",
-      duration: 1
+      backgroundColor: '#ffffff',
+      duration: 1,
     },
     {
-      backgroundColor: "#ccd1b2",
-      duration: 1
+      backgroundColor: '#ccd1b2',
+      duration: 1,
     }
   );
 });
-    
-    // Dark to Light Color Change
-$("[colourchangeback]").each(function (index) {
-  let triggerElement = $(this);
-  let targetElement = $(".page_wrap");
+
+// Dark to Light Color Change
+document.querySelectorAll('[colourchangeback]').forEach(function (triggerElement) {
+  let targetElement = document.querySelector('.page_wrap');
 
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: triggerElement,
-      // trigger element - viewport
-      // can also use "20px 80%"
-      start: "top bottom",
-      end: "top center",
-      scrub: 1
-    }
+      start: 'top bottom',
+      end: 'top center',
+      scrub: 1,
+    },
   });
+
   tl.fromTo(
-    targetElement,
+    targetElement.style,
     {
-      backgroundColor: "#ccd1b2",
-      duration: 1
+      backgroundColor: '#ccd1b2',
+      duration: 1,
     },
     {
-      backgroundColor: "#ffffff",
-      duration: 1
+      backgroundColor: '#ffffff',
+      duration: 1,
     }
   );
 });
+
     
 
 
