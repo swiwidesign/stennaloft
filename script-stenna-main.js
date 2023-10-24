d8b6a276d55676cb606979307c70b77492752f9f
swiwidesign <hello@swiwi.me>
2023-10-24 23:36:05 +0200

    Update script-stenna-main.js

--- script-stenna-main.js
+++ script-stenna-main.js
@@ -74,8 +74,8 @@ document.querySelectorAll("[colourchange]").forEach(function (triggerElement) {
     scrollTrigger: {
       trigger: triggerElement,
       scrub: true,
-      start: "top 75%",
-      end: "top center",
+      start: "top center",
+      end: "bottom bottom",
     },
   });
 
