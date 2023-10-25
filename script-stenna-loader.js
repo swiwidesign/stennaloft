1b891b1686e8055e35a710db977deb8815725304
swiwidesign <hello@swiwi.me>
2023-10-25 16:36:16 +0200

    Update script-stenna-loader.js

--- script-stenna-loader.js
+++ script-stenna-loader.js
@@ -1,47 +1,50 @@
 // PAGE TRANSITION
 // Code that runs on pageload
-window.onload = function() {
-  gsap.to(".loader", {
-    opacity: 0,
-    duration: 1,
-    onComplete: () => {
-      gsap.set(".loader", { display: "none" });
-    }
-  });
+window.onload = function () {
+    gsap.to(".loader", {
+        opacity: 0,
+        duration: 1,
+        onComplete: () => {
+            gsap.set(".loader", {
+                display: "none"
+            });
+        }
+    });
 }
 
 // Code that runs on click of a link
 $(document).ready(function () {
-  $("a").on("click", function (e) {
-    if (
-    	$(this).prop("hostname") === window.location.host &&
-      $(this).attr("href").indexOf("#") === -1 &&
-      $(this).attr("target") !== "_blank") {
-        e.preventDefault();
-        let destination = $(this).attr("href");
-        gsap.set(".loader", { display: "block" });
-        gsap.fromTo(
-          ".loader",
-          {
-            opacity: 0
-          },
-          {
-            opacity: 1,
-            onComplete: () => {
-              window.location = destination;
-            }
-          }
-        );
+    $("a").on("click", function (e) {
+        if (
+            $(this).prop("hostname") === window.location.host &&
+            $(this).attr("href").indexOf("#") === -1 &&
+            $(this).attr("target") !== "_blank") {
+            e.preventDefault();
+            let destination = $(this).attr("href");
+            gsap.set(".loader", {
+                display: "block"
+            });
+            gsap.fromTo(
+                ".loader", {
+                    opacity: 0
+                }, {
+                    opacity: 1,
+                    duration: 1,
+                    onComplete: () => {
+                        window.location = destination;
+                    }
+                }
+            );
+        }
+    });
+
+
+
+
+    // On click of the back button
+    window.onpageshow = function (event) {
+        if (event.persisted) {
+            window.location.reload();
+        }
     }
-  });
-  
-  
-  
-  
-  // On click of the back button
-  window.onpageshow = function(event){
-  	if (event.persisted) {
-    	window.location.reload();
-    }
-  }
 });
