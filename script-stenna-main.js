b00b1e5aa49ea7d5c3dc5ee8e6a8c147e24efe0a
swiwidesign <hello@swiwi.me>
2023-10-25 13:44:47 +0200

    Update script-stenna-main.js

--- script-stenna-main.js
+++ script-stenna-main.js
@@ -57,11 +57,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     // Hero Images
     gsap.fromTo(".hero_image", {
-        clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100% )",
+        clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)",
     }, {
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
         delay: 0.2,
-        stagger: 0.8,
+        stagger: {
+            amount: 1.5,
+            each: -0.1,
+        },
         ease: "circ.inOut",
 
     });
