f9f32501a007ca6b8fd4444076c45a02adea0df3
swiwidesign <hello@swiwi.me>
2023-10-25 12:51:04 +0200

    Update script-stenna-main.js

--- script-stenna-main.js
+++ script-stenna-main.js
@@ -62,7 +62,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
         delay: 0.2,
         stagger: 0.6,
-        ease: "power3.out",
+        ease: "power4.out",
 
     });
 
